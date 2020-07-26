bb34bbacbd644c1e1ab63f117e9873c3125352db
Toms Veidemanis <toms@chess24.com>
2020-07-27 00:19:10 +0200

    ✨ Q1: print city

--- promises.js
+++ promises.js
@@ -1,13 +1,18 @@
 class Provider {
     static getWeather(city) {
         return Promise.resolve(`The weather of ${city} is Cloudy`);
-    }
+    };
 
     static getLocalCurrency(city) {
         return Promise.resolve(`The local currency of ${city} is GBP`);
-    }
+    };
 
     static findCity(long, lat) {
         return Promise.resolve('London')
-    }
+    };
 }
+
+Provider.findCity(51.5074, 0.1278)
+    .then((city) => console.log(city))
+    .catch((e) => console.log(e));
+
