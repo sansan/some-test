839a8f04ec02bdafd65eb3297bf79cec19b560ce
Toms Veidemanis <toms@chess24.com>
2020-07-27 00:10:37 +0200

    ✨SECTION2: initialize

---
+++ promises.js
@@ -0,0 +1,13 @@
+class Provider {
+    static getWeather(city) {
+        return Promise.resolve(`The weather of ${city} is Cloudy`);
+    }
+
+    static getLocalCurrency(city) {
+        return Promise.resolve(`The local currency of ${city} is GBP`);
+    }
+
+    static findCity(long, lat) {
+        return Promise.resolve('London')
+    }
+}
